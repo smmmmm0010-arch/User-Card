8e965d3df1b949304722e6cfa70620faa64e68ac
smmmmm0010-arch <smmmmm0010@gmail.com>
2026-03-10 23:14:17 +0300

    Update Main.kt

--- Main.kt
+++ Main.kt
@@ -2,16 +2,16 @@
 
 fun main() {
 
-    val name = "Mona"
+    val name = "Shahd"
     val nickname: String? = null
     val age = 20
 
     println("Name: $name")
 
-    // null safety
+   
     println("Nickname: ${nickname ?: "No nickname"}")
 
-    // check age
+   
     val status = if (age >= 18) "Adult" else "Minor"
 
     println("Age: $age")
